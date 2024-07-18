4c1c30e71317af14001dfaad5b938f5515b81fe2
landakdu <durilandak429@gmail.com>
2024-07-18 17:48:06 +0800

    Update keterangan.js

--- keterangan.js
+++ keterangan.js
@@ -1,11 +1,11 @@
 const bgbahaya = document.getElementsByClassName("bgbahaya");
 for (let i = 0; i < bgbahaya.length; i++) {
-  bgbahaya[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf link terbaru silahkan minta kepada cs melalui tombol <span class='color-red'>CHAT</span> yang telah kami sediakan di bawah, kami kecewa dengan situs ini alasannya tidak akan kami sebar luaskan maka dari itu link tanpa vpn tidak kami sediakan dan akhir kata kami doakan yang terbaik untuk anda semoga tetap aman dan nyaman bermain di situs ini.</h1>";
+  bgbahaya[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf link terbaru silahkan minta kepada cs melalui tombol <span class='color-red'>CHAT</span> yang telah kami sediakan di bawah, situs ini sangat mengecewakan menurut judisuhu alasannya tidak akan di sebar oleh judisuhu untuk link tanpa vpn tidak di sedikan lagi oleh judisuhu, anda harus berhati-hati jika tetap ingin melanjutkan kami doakan yang terbaik untuk anda semoga tetap aman dan nyaman bermain di situs tersebut, salam dari judisuhu 🙏.</h1>";
 }
 
 const bgwaspada = document.getElementsByClassName("bgwaspada");
 for (let i = 0; i < bgwaspada.length; i++) {
-  bgwaspada[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf link terbaru silahkan minta kepada cs melalui tombol <span class='color-red'>CHAT</span> yang telah kami sediakan di bawah, kami ragu dengan situs ini jika tetap ingin bermain anda harus <span class='color-red'>WASPADA</span> karena bisa saja situs ini <span class='color-red'>tidak aman</span> dan untuk sementara link tanpa vpn tidak kami sediakan sampai situs ini kami anggap aman untuk di mainkan</h1>";
+  bgwaspada[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf link terbaru silahkan minta kepada cs melalui tombol <span class='color-red'>CHAT</span> yang telah kami sediakan di bawah, judisuhu ragu dengan situs ini jika tetap ingin melanjutkan bermain anda harus <span class='color-red'>WASPADA</span> karena bisa saja situs ini <span class='color-red'>tidak aman</span> dan untuk sementara link tanpa vpn tidak di sediakan judisuhu sampai situs ini di anggap aman untuk di mainkan</h1>";
 }
 
 const bgdomain = document.getElementsByClassName("bgdomain");
