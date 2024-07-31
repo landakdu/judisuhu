b34be88ee98ec18521e77fdfb85181bbc086fbfd
landakdu <durilandak429@gmail.com>
2024-07-31 23:13:50 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -18,7 +18,7 @@ function xskslot() {location.replace("https://sklahbray.com/register?ref=rakuyab
 function xibosport() {location.replace("https://v2ibocuan.store/");}
 function xpiontogel() {location.replace("https://pionnaga.com/link.php?member=rakuyaba");}
 function xdauntogel() {location.replace("https://d4unpucuk.com/link.php?member=rakuyaba");}
-function xkenzo188() {location.replace("https://kenzo188url.top/register?ref=A3MMMAF4PO7");}
+function xkenzo188() {location.replace("https://kenzo188team.com/register?ref=A3MMMAF4PO7");}
 function xpmg88() {location.replace("https://pmg88.store/register?ref=u2y78Bp");}
 function xnyaitogel() {location.replace("https://nyaispin.org/link.php?member=rakuyaba");}
 function xpintutogel() {location.replace("https://pintumegawin.com/register?referral_code=rakuyaba");}
