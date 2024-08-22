4424e34d75d40b1c3ee273112cf157c7a86413c9
landakdu <durilandak429@gmail.com>
2024-08-22 20:34:24 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -71,7 +71,7 @@ function xgacorslot() {location.replace("https://gacorslotoke.site/register/WQ8K
 function xbom29toto() {location.replace("https://b29milk.com/link.php?member=rakuyaba");}
 function xharbet35() {location.replace("https://harbet35cuan.site/?ref=rakuyaba");}
 function xtokekwin() {location.replace("http://xn--c1abejboayi.xn--mk1bu44c/daftar?ref=rakuyaba");}
-function xdewakoin() {location.replace("https://dewakoin-daftar5.shop/register?ref=PABAAAP0MH8");}
+function xdewakoin() {location.replace("https://dewakoin-vip30.quest/register?ref=PABAAAP0MH8");}
 function xdiva168() {location.replace("https://diva168best.com/register?referral=15542094924");}
 function xroyal123() {location.replace("https://www.royal123fk.com/register?ref=F88M1AN0ZJ2");}
 function xaquaslot() {location.replace("https://aquaslot15.com/?ref=bunsin");}
