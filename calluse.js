ee2fea108fe01489823ee7bc5e3f4ed5ad0ec782
landakdu <durilandak429@gmail.com>
2024-09-20 07:03:50 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -82,4 +82,4 @@ function xfreebet() {location.replace("https://freebetdemo.blogspot.com/");}
 function xbetcoinasia() {location.replace("https://linkbtc.pro/?ref=bunsin");}
 function xjangkaslot() {location.replace("https://noblockss.blogspot.com/");}
 function xtogeltoto() {location.replace("https://noblockss.blogspot.com/");}
-function xpphoki() {location.replace("https://pphokivip.cam/?ref=rakuyaba");}
+function xpphoki() {location.replace("https://pphokigacor.com/?ref=rakuyaba");}
