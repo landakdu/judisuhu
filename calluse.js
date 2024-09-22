4cc11e8a22fe5684f38e7adf8d4d83d08a9dd586
landakdu <durilandak429@gmail.com>
2024-09-23 07:06:37 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -45,7 +45,7 @@ function xmpo08() {location.replace("https://mpo08hold.com/register/WM418MNP");}
 function xdewa234() {location.replace("https://silvermile.net/register/W42BG936");}
 function xmpo666() {location.replace("https://mpo666fu.com/register/WM41P433");}
 function xmpo555() {location.replace("https://mpo555-smart.xyz/register/WR3N6NQ9");}
-function xkadobet() {location.replace("https://jobkadobet.com/");}
+function xkadobet() {location.replace("https://jobkadobet.com/?ref=rakuyaba");}
 function xsweet777() {location.replace("https://lgo777.beauty/register?ref=GS2AEAB0SVJ");}
 function xkas138() {location.replace("https://kas138vip.com/?ref=rakuyaba");}
 function xjudi388() {location.replace("https://jd388aja.top/register?reff=rakuyaba");}
