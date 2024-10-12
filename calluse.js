d3c645babb5c1bc9434542d13d12ea64e175060a
landakdu <durilandak429@gmail.com>
2024-10-12 19:28:43 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -52,7 +52,7 @@ function xkas138() {location.replace("https://kas138vip.com/?ref=rakuyaba");}
 function xjudi388() {location.replace("https://jd388aja.top/register?reff=rakuyaba");}
 function xmega55() {location.replace("https://mega55bisa.xyz/register?ref=ZGGIGAB00JK");}
 function x66kone() {location.replace("https://t.me/W66KONE");}
-function xmpo222() {location.replace("https://mpo222man.com/register/WNR6893G");}
+function xmpo222() {location.replace("https://mpo222sparkler.com/register/WNR6893G");}
 function xcoblos4d() {location.replace("https://sobatcoblos4d.one/?ref=rakuyaba");}
 function xagenslot168() {location.replace("https://agenslt168.xyz/register/Y7G7GR7B");}
 function xnewliga365() {location.replace("https://newliga365t.pro/?action=register&rc=N8SY7QO738&ci=11");}
