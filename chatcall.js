bf1817443ad0e815295469276e465e52f60c169d
landakdu <durilandak429@gmail.com>
2024-10-14 05:46:26 +0800

    Add files via upload

---
+++ chatcall.js
@@ -0,0 +1,23 @@
+// code standing inside html.js for easy edit
+const menu1 = `
+<button class="tutup" onClick="window.location.reload();">TUTUP CHAT</button>
+`;
+
+const menu2 = `
+<img class="iklan1" onclick="document.location='../../index.html'" style="background-image: url(https://i.ibb.co/QC3Nq9T/border1.png), url(https://i.pinimg.com/564x/a9/f1/eb/a9f1eb1fca8c87730ac3e2af83be4f8e.jpg);" src="https://i.ibb.co/8c8MZYB/jdiklan.gif"></img>
+`;
+
+const menu3 = `
+<img class="iklan2" onclick="document.location='../../index.html'" style="background-image: url(https://i.ibb.co/QC3Nq9T/border1.png), url(https://i.pinimg.com/600x/7b/1c/c0/7b1cc0ad4c481630bc683d38993efd4b.jpg);" src="https://i.ibb.co/8c8MZYB/jdiklan.gif"></img>
+
+`;
+
+const menu4 = `
+<button class="buka" onclick="enableAutoplay(), showhide(), tombol()" value="button"><a class="buka1"><span>MULAI CHAT</span></a></button>
+<button class="buka0" onclick="document.location='../../index.html'" value="button">JUDISUHU</button>
+`;
+
+const menu5 = `
+
+`;
+
---
+++ chatjs.js
@@ -0,0 +1,24 @@
+var x = document.getElementById("autoplaymusic");
+function enableAutoplay() { 
+  var url = ("https://audio.jukehost.co.uk/JLWHIcrVuQmXXrY3TUEiV9owZtmeoTux")
+        var audio = new Audio(url);
+        audio.play();
+}
+
+function showhide() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function tombol() {
+  var x = document.getElementById("mybutton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
