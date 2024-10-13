c2394dea599b749342596a63c5c95ed03d2ed58d
landakdu <durilandak429@gmail.com>
2024-10-14 03:27:11 +0800

    Add files via upload

---
+++ keterangan.js
@@ -0,0 +1,19 @@
+const bgbahaya = document.getElementsByClassName("bgbahaya");
+for (let i = 0; i < bgbahaya.length; i++) {
+  bgbahaya[i].innerHTML = "<h1 class='keterangan-font-size'><span class='color-red'>SITUS INI BERBAHAYA</span>, sebab tidak dapat di temukan kemungkin telah tutup ataupun berganti nama, di takutkan ini adalah situs scam yang dapat menghilang kapan saja dan merubah nama situs kapan saja, hal ini sangat berbahaya dampaknya untuk pemain dan untuk mencegah hal yang tidak di inginkan lebih lanjut kami tidak mencantumkan link ataupun chat situs ini demi menjaga kenyamanan anda atas perhatiannya kami ucapkan terima kasih 🙏.</h1>";
+}
+
+const bgwaspada = document.getElementsByClassName("bgwaspada");
+for (let i = 0; i < bgwaspada.length; i++) {
+  bgwaspada[i].innerHTML = "<h1 class='keterangan-font-size'>Mohon maaf situs ini adalah salah satu situs yang belum di nyatakan aman oleh kami karena beberapa alasan, <span class='color-yellow'>1. belum kami coba</span>, <span class='color-red'>2. situs tidak membayar kemenangan</span>, <span class='color-yellow'>3. situs tidak memberikan hak pemain seperti yang di promosikan</span>, <span class='color-red'>4. situs tidak menjaga keamanan akun pemain</span>, salah satu contohnya customer service akan meresetkan password baru akun anda untuk siapa saja yang dapat memberikan data nama serta nomor rekening anda hal ini akan sangat memudahkan hacker abal abal mendapatkan akun dan melakukan withdraw ataupun transfer chip jika masih ada saldo di dalam akun anda, data rekening anda sendiri tentu sangat mudah di dapatkan bisa dari penjual data base, cek rekening secara acak di bank, admin pinjol, dan mungkin juga karyawan bank yang nakal.</br>Intinya situs ini tidak kami rekomendasikan untuk saat ini sebab bisa saja terjadi hal yang tidak di inginkan kepada anda, namun kami tetap menyediakan sarana komunikasi <span class='color-green'>LiveChat</span> situs ini agar anda tetap bisa bekomunikasi dengan situs ini secara langsung, atas perhatiannya kami ucapkan terima kasih 🙏.</h1>";
+}
+
+const bgdomain = document.getElementsByClassName("bgdomain");
+for (let i = 0; i < bgdomain.length; i++) {
+  bgdomain[i].innerHTML = "<h1 class='keterangan-font-size'>Tombol berwarna <span class='color-green'>HIJAU</span> artinya situs tersebut <span class='color-green'>AMAN</span> dan link tanpa vpn dapat di gunakan selamanya karena otomatis update setiap hari<br/><br/>Tombol berwarna <span class='color-yellow'>KUNING</span> dan <span class='color-red'>MERAH</span> artinya situs tersebut tidak aman menurut judisuhu dan link tanpa vpn tidak di sediakan judisuhu, jika membutuhkan link terbaru klik tombol <span class='color-red'>CHAT</span> yang di sediakan judisuhu agar anda dapat berkomunikasi dan meminta link terbaru secara langsung ke situs resminya, tombol <span class='color-red'>CHAT</span>  hanya muncul jika anda mengklik tombol <span class='color-yellow'>KUNING</span> atau<span class='color-red'>MERAH</span>sesuai dengan situs yang anda inginkan.</h1>";
+}
+
+const bgchatadmin = document.getElementsByClassName("bgchatadmin");
+for (let i = 0; i < bgchatadmin.length; i++) {
+  bgchatadmin[i].innerHTML = "<h1 class='keterangan-font-size'>CHAT KE ADMIN JUDISUHU</br></br><span class='color-blue'>CHAT FACEBOOK :</span> di gunakan untuk berkomunikasi dengan admin dan akan di balas saat admin online.</br></br><span class='color-green'>CHAT TAWK :</span> di gunakan untuk mengirimkan pesan singkat untuk admin tanpa membutuhkan balasan chat dari admin biasanya di pergunakan untuk request situs yang belum tersedia atau meminta admin mengupdate link baru</br></br>Contoh 1 pesan singkat</br>admin update link terbaru situs ABCD</br></br>Contoh 2 pesan singkat</br>admin tambahkan situs 1234 agar saya dapat masuk situsnya tanpa vpn selamanya</br></br>Intinya sampaikan saja seluruh keinginan anda dalam 1 kali pengiriman pesan nanti jika sudah di baca oleh admin akan di kerjakan.</h1>";
+}
