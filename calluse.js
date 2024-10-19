349674f67d0707cb815c12a36a2248c09d672c58
landakdu <durilandak429@gmail.com>
2024-10-19 19:40:28 +0800

    Update calluse.js

--- calluse.js
+++ calluse.js
@@ -19,7 +19,7 @@ function xjet88bet() {location.replace("https://jet88betcore.com/?ref=rakuyaba")
 function xmpo369() {location.replace("https://mpo369ax.com/register/W7N46140");}
 function xnewliga365() {location.replace("https://newliga365d.info/?action=register&rc=N8SY7QO738&ci=11");}
 function x8bettingan() {location.replace("https://bettingan3.biz/?ref=QZPZPBE00O4");}
-function xsweet777() {location.replace("https://juragan777.cyou/register?ref=GS2AEAB0SVJ");}
+function xsweet777() {location.replace("https://www.juragan777.boats/register?ref=GS2AEAB0SVJ");}
 function xcoblos4d() {location.replace("https://sobatcoblos4d.one/?ref=rakuyaba");}
 function xmpo08() {location.replace("https://mpo08seribu.com/register/WM418MNP");}
 function xkadobet() {location.replace("https://kadobetjob.com/?ref=rakuyaba");}
