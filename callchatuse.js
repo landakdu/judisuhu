fb6c584235e652f07ff8ef828c83d600d3da2cf2
landakdu <durilandak429@gmail.com>
2024-11-04 10:21:19 +0800

    Delete callchatuse.js

--- callchatuse.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { document.createElement('form').submit.call(document.getElementById('yourform')); }); 
-
-function lcdewatogel() {location.replace("https://judisuhu.store/chat/dewatogel/");}
-function lcmpo888() {location.replace("https://judisuhu.store/chat/mpo888/");}
-function lcsbobet() {location.replace("https://judisuhu.store/chat/sbobet/");}
-function lchokiwin() {location.replace("https://judisuhu.store/chat/hokiwin/");}
-function lcmpocasino() {location.replace("https://judisuhu.store/chat/mpocasino/");}
-
